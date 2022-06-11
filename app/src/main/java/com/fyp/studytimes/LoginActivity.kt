ec433583be939618390c04d72eb3b06080a95d6a
sleepywww <yapjj-wp19@student.tarc.edu.my>
2022-06-11 15:24:14 +0800

    Initialize Code

--- app/src/main/java/com/fyp/studytimes/LoginActivity.kt
+++ app/src/main/java/com/fyp/studytimes/LoginActivity.kt
@@ -1,11 +1,34 @@
 package com.fyp.studytimes
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class LoginActivity : AppCompatActivity() {
+    private var loginEmail: EditText? = null
+    private var loginPassword: EditText? = null
+    private var loginBtn: Button? = null
+    private var registerHere: TextView? = null
+    private var pDialog: ProgressDialog? = null
+    private var userAuth: FirebaseAuth? = null
+    private var databaseRef: DatabaseReference? = null
+    private var loggedInUser: UserModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (CometChat.getLoggedInUser() != null) {
+            goToMainActivity();
+        }
     }
 }
--- app/src/main/java/com/fyp/studytimes/SplashScreenActivity.kt
+++ app/src/main/java/com/fyp/studytimes/SplashScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.fyp.studytimes
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
@@ -9,5 +11,11 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
     }
 }
