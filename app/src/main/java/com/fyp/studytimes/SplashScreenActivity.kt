4b2a66402a2140734a5c1c156be75cd36c17673d
sleepywww <yapjj-wp19@student.tarc.edu.my>
2022-06-11 15:46:09 +0800

    Splash Screen Done

--- app/src/main/java/com/fyp/studytimes/SplashScreenActivity.kt
+++ app/src/main/java/com/fyp/studytimes/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
@@ -12,10 +13,15 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 3000)
     }
 }
