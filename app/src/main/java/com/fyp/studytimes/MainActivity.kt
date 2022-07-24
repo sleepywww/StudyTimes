ac6e4833de499d597e11c9c8350993db070a272a
sleepywww <yapjj-wp19@student.tarc.edu.my>
2022-07-24 13:47:50 +0800

    Bottom Navigation Bar Function Added

--- app/src/main/java/com/fyp/studytimes/MainActivity.kt
+++ app/src/main/java/com/fyp/studytimes/MainActivity.kt
@@ -1,11 +1,32 @@
+@file:Suppress("DEPRECATION")
+
 package com.fyp.studytimes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_dashboard -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.navBottom)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
